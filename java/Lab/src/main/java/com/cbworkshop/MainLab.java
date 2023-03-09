1042cd437b78471e94c9a3004649fcb1c9e6335d
Tayeb Chlyah <tayebchlyah@gmail.com>
2023-03-09 17:43:23 +0400

    Update Java Lab for Collections

--- java/Lab/src/main/java/com/cbworkshop/MainLab.java
+++ java/Lab/src/main/java/com/cbworkshop/MainLab.java
@@ -1,11 +1,11 @@
 package com.cbworkshop;
 
-import com.couchbase.client.java.Cluster;
-import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.*;
 import com.couchbase.client.java.json.JsonObject;
-import reactor.core.publisher.Flux;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MainLab {
@@ -20,11 +20,14 @@ public class MainLab {
     public static final String CMD_QUERY_REACTIVE = "queryreactive";
     public static final String CMD_QUERY_AIRPORTS = "queryairports";
     public static final String CMD_BULK_WRITE = "bulkwrite";
-    public static final String CMD_BULK_WRITE_SYNC = "bulkwritesync";
+    public static final String CMD_BULK_WRITE_REACTIVE = "bulkwritereactive";
     public static final String CMD_SEARCH = "search";
+    public static final String SCOPE = "inventory";
+    public static final String MSG = "msg";
 
     private static Cluster cluster = null;
-    private static Collection collection = null;
+    private static Scope scope;
+    private static Collection msgCollection;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -59,6 +62,7 @@ public class MainLab {
         switch (words[0].toLowerCase()) {
             case CMD_QUIT:
                 System.out.println("bye!");
+                System.exit(0);
                 break;
             case CMD_CREATE:
                 create(words);
@@ -87,8 +91,8 @@ public class MainLab {
             case CMD_BULK_WRITE:
                 bulkWrite(words);
                 break;
-            case CMD_BULK_WRITE_SYNC:
-                bulkWriteSync(words);
+            case CMD_BULK_WRITE_REACTIVE:
+                bulkWriteReactive(words);
                 break;
             case CMD_SEARCH:
                 search(words);
@@ -102,29 +106,35 @@ public class MainLab {
     }
 
     private static void create(String[] words) {
-        String key = "msg::" + words[1];
+        String key = words[1];
         String from = words[2];
         String to = words[3];
         // TODO: Lab
+        System.out.printf("Document created in collection '%s' with key: '%s'%n", MSG, key);
     }
 
     private static void read(String[] words) {
-        String key = words[1];
+        String collection = words[1];
+        String key = words[2];
         // TODO: Lab
     }
 
     private static void update(String[] words) {
         String key = "airline_" + words[1];
         // TODO: Lab
+        System.out.printf("Document updated in collection '%s' with key: '%s'%n", MSG, key);
     }
 
     private static void subdoc(String[] words) {
+        String key = words[1];
         // TODO: Lab
+        System.out.printf("Sub Document updated in collection '%s' with key: '%s'%n", MSG, key);
     }
 
     private static void delete(String[] words) {
-        String key = "msg::" + words[1];
+        String key = words[1];
         // TODO: Lab
+        System.out.printf("Document deleted in collection '%s' with key: '%s'%n", MSG, key);
     }
 
     private static void query() {
@@ -141,39 +151,34 @@ public class MainLab {
         // TODO: Lab
     }
 
-    private static void bulkWrite(String[] words) {
+    private static Map<String, JsonObject> prepareMsgs(String[] words) {
         int size = Integer.parseInt(words[1]);
 
         System.out.println("Deleting messages ...");
-        cluster.query("DELETE FROM `travel-sample` WHERE type='msg'");
+        cluster.query("DELETE FROM `travel-sample`.inventory.msg");
 
-        System.out.printf("Writing %d messages%n", size);
-        long ini = System.currentTimeMillis();
-        Flux.range(0, size)
-                .flatMap(i -> collection.reactive().insert("msg::" + i, JsonObject.create()
+        Map<String, JsonObject> msgs = IntStream.range(0, size)
+                .boxed()
+                .collect(Collectors.toMap(Object::toString, i -> JsonObject.create()
                         .put("timestamp", System.currentTimeMillis())
                         .put("from", "me")
-                        .put("to", "you")
-                        .put("type", "msg")))
-                .blockLast();
+                        .put("to", "you")));
+
+        System.out.printf("Writing %d messages%n", msgs.size());
+        return msgs;
+    }
+
+    private static void bulkWriteReactive(String[] words) {
+        Map<String, JsonObject> msgs = prepareMsgs(words);
+        long ini = System.currentTimeMillis();
+        // TODO: Lab
         System.out.printf("Time elapsed %d ms%n", System.currentTimeMillis() - ini);
     }
 
-    private static void bulkWriteSync(String[] words) {
-        int size = Integer.parseInt(words[1]);
-
-        System.out.println("Deleting messages ...");
-        cluster.query("DELETE FROM `travel-sample` WHERE type='msg'");
-
-        System.out.printf("Writing %d messages%n", size);
+    private static void bulkWrite(String[] words) {
+        Map<String, JsonObject> msgs = prepareMsgs(words);
         long ini = System.currentTimeMillis();
-        IntStream.range(0, size)
-                .forEach(i -> collection.insert("msg::" + i, JsonObject.create()
-                        .put("timestamp", System.currentTimeMillis())
-                        .put("from", "me")
-                        .put("to", "you")
-                        .put("type", "msg")));
-
+        // TODO: Lab
         System.out.printf("Time elapsed %d ms%n", System.currentTimeMillis() - ini);
     }
 
@@ -188,11 +193,19 @@ public class MainLab {
     }
 
     private static void usage() {
-        System.out.println("Usage options: \n\n" + CMD_CREATE + " [key from to] \n" + CMD_READ + " [key] \n"
-                + CMD_UPDATE + " [airline_key] \n" + CMD_SUBDOC + " [msg_key] \n" + CMD_DELETE + " [msg_key] \n"
-                + CMD_QUERY + " \n" + CMD_QUERY_AIRPORTS + " [sourceairport destinationairport] \n"
-                + CMD_QUERY_REACTIVE + " \n" + CMD_BULK_WRITE + " [size] \n" + CMD_BULK_WRITE_SYNC + " [size] \n"
-                + CMD_SEARCH + " [term] \n" + CMD_QUIT);
+        System.out.println("Usage options:\n");
+        System.out.printf("%s [msg_key from to]\n", CMD_CREATE);
+        System.out.printf("%s [collection key]\n", CMD_READ);
+        System.out.printf("%s [airline_key]\n", CMD_UPDATE);
+        System.out.printf("%s [msg_key]\n", CMD_SUBDOC);
+        System.out.printf("%s [msg_key]\n", CMD_DELETE);
+        System.out.printf("%s\n", CMD_QUERY);
+        System.out.printf("%s\n", CMD_QUERY_REACTIVE);
+        System.out.printf("%s [sourceairport destinationairport]\n", CMD_QUERY_AIRPORTS);
+        System.out.printf("%s [size]\n", CMD_BULK_WRITE);
+        System.out.printf("%s [size]\n", CMD_BULK_WRITE_REACTIVE);
+        System.out.printf("%s [term]\n", CMD_SEARCH);
+        System.out.println(CMD_QUIT);
     }
 
 }
